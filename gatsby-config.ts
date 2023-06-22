45d84ef209346205d19ea9dc829320790bf78632
Martti Grönholm <martti.gronholm@vincit.fi>
2023-06-22 15:18:35 +0300

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,20 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Portfolio Website`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: 'gatsby-source-datocms',
+    options: {
+      "apiToken": "8175a773be9c08732070506f79673e"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass"]
+};
+
+export default config;
