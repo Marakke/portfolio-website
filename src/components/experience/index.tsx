62a48f8010a81e2ab2f228e462ceeed7ec15ea22
Martti <martti.gronholm@gmail.com>
2025-01-20 20:13:35 +0200

    Add certifications section

--- src/components/experience/index.tsx
+++ src/components/experience/index.tsx
@@ -24,6 +24,14 @@ const Experience = () => {
             endYear
           }
         }
+        certificateList {
+          title
+          certifications {
+            name
+            issueDate
+            link
+          }
+        }
       }
     }
   `)
@@ -32,27 +40,43 @@ const Experience = () => {
 
   return (
     <div className="experience-container" id="experience" data-testid="experience-container">
-      <div className="education-container">
-        <h2>{experience.education.title}</h2>
-        <div className="education-content">
-          <h3 className="university">{experience.education.university}</h3>
-          <div className="programmes">
-            {experience.education.programmes.map(( programme: any ) => (
-              <div className="programme" key={programme.name}>
-                <p className="name">{programme.name}</p>
-                <p className="years">{programme.startYear} {"->"} {programme.endYear}</p>
+      <div className="work-and-education">
+        <div className="education-container">
+          <h2>{experience.education.title}</h2>
+          <div className="education-content">
+            <h3 className="university">{experience.education.university}</h3>
+            <div className="programmes">
+              {experience.education.programmes.map(( programme: any ) => (
+                <div className="programme" key={programme.name}>
+                  <p className="name">{programme.name}</p>
+                  <p className="years">{programme.startYear} {"->"} {programme.endYear}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="work-experience-container">
+          <h2>{experience.workExperience.title}</h2>
+          <div className="work-places">
+            {experience.workExperience.workPlaces.map(( workPlace: any ) => (
+              <div className="work-place" key={workPlace.name}>
+                <h3 className="name">{workPlace.name}</h3>
+                <p className="years">{workPlace.startYear} {"->"} {workPlace.endYear}</p>
               </div>
             ))}
           </div>
         </div>
       </div>
-      <div className="work-experience-container">
-        <h2>{experience.workExperience.title}</h2>
-        <div className="work-places">
-          {experience.workExperience.workPlaces.map(( workPlace: any ) => (
-            <div className="work-place" key={workPlace.name}>
-              <h3 className="name">{workPlace.name}</h3>
-              <p className="years">{workPlace.startYear} {"->"} {workPlace.endYear}</p>
+      <div className="certifications">
+        <h2>{experience.certificateList.title}</h2>
+        <div className="certificate-list">
+          {experience.certificateList.certifications.map(( certification: any ) => (
+            <div className="certificate" key={certification.name}>
+              <h3 className="name">{certification.name}</h3>
+              <p className="issue-date">{certification.issueDate}</p>
+              <a className="link" href={certification.link} target="_blank" rel="noreferrer">
+                Show credential
+              </a>
             </div>
           ))}
         </div>
